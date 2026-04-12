a55a49bd335af0a8a16ce6a2082b50b2be4c9d3c
lior25659567 <lior2565967@gmail.com>
2026-04-12 20:33:27 +0300

    Update case studies (12/04/2026, 20:33)

--- src/data/case-studies/index.js
+++ src/data/case-studies/index.js
@@ -1,8 +1,10 @@
 // Auto-generated — do not edit manually
 import cs0 from './itero-scan-workflow.json';
-import cs1 from './wizecare.json';
+import cs1 from './project-1776014998709.json';
+import cs2 from './wizecare.json';
 
 export const savedCaseStudies = {
   'itero-scan-workflow': cs0,
-  'wizecare': cs1,
+  'project-1776014998709': cs1,
+  'wizecare': cs2,
 };
